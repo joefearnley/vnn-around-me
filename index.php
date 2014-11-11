c508aa5236e04ab0b340a28aef1f25997f45cf9b
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-10 19:15:52 -0500

    Removing config stuff

--- index.php
+++ index.php
@@ -4,4 +4,9 @@ require 'vendor/autoload.php';
 
 $app = new \Slim\Slim();
 
-
+ActiveRecord\Config::initialize(function($config) {
+    $config->set_model_directory('models');
+    $config->set_connections([
+        'production' => 'mysql://vnn:password@localhost/vnnaroundme'
+    ]);
+});
