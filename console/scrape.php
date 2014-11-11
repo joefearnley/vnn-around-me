851d19e704d7cf654543f28c8f18be855ad7ae35
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-10 19:24:57 -0500

    Converting over scrape command

--- console/scrape.php
+++ console/scrape.php
@@ -1,82 +1,29 @@
-<?php namespace App\Console;
-
-use Illuminate\Console\Command;
-use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Input\InputArgument;
+<?php
 
 use Goutte\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-class VnnScraperCommand extends Command {
+$this->info("Clearing schools.csv file.\n");
+\File::put(storage_path() . '/temp/schools.csv', '');
 
-	/**
-	 * The console command name.
-	 *
-	 * @var string
-	 */
-	protected $name = 'vnn:scrape';
+$url = 'http://varsitynewsnetwork.com/vnn-partner-schools/';
 
-	/**
-	 * The console command description.
-	 *
-	 * @var string
-	 */
-	protected $description = 'Scrape VNN sites for address.';
+echo "Crawling site.\n";
+$client = new \Goutte\Client();
+$crawler = $client->request('GET', $url);
 
-	/**
-	 * Create a new command instance.
-	 *
-	 * @return void
-	 */
-	public function __construct()
-	{
-		parent::__construct();
+$crawler->filter('.member_state')->each(function($node) {
+	$stateName = $node->filter('h3')->text();
+
+	$list = $node->filter('ul > li a');
+	foreach ($list as $node) {
+		$schoolName = $node->nodeValue;
+		$schoolUrl = $node->getAttribute('href');
+
+		// write to the csv file
+		$line = $schoolName . "," . $stateName . "," . $schoolUrl . "\n";
+		\File::append(storage_path() . '/temp/schools.csv', $line);
 	}
+});
 
-	/**
-     * Client for crawler
-     *
-     * @var Client
-     */
-    private $client;
-
-    /**
-     * URL for page to crawl.
-     *
-     * @var string
-     */
-    private $url;
-
-		/**
-		 * Execute the console command.
-		 *
-		 * @return mixed
-		 */
-		public function fire()
-		{
-			$this->info("Clearing schools.csv file.\n");
-			\File::put(storage_path() . '/temp/schools.csv', '');
-
-			$this->url = 'http://varsitynewsnetwork.com/vnn-partner-schools/';
-
-			$this->info("Crawling site.\n");
-			$this->client = new \Goutte\Client();
-			$crawler = $this->client->request('GET', $this->url);
-
-			$crawler->filter('.member_state')->each(function($node) {
-				$stateName = $node->filter('h3')->text();
-
-				$list = $node->filter('ul > li a');
-				foreach ($list as $node) {
-					$schoolName = $node->nodeValue;
-					$schoolUrl = $node->getAttribute('href');
-
-					// write to the csv file
-					$line = $schoolName . "," . $stateName . "," . $schoolUrl . "\n";
-					\File::append(storage_path() . '/temp/schools.csv', $line);
-				}
-			});
-
-			$this->info("File written\n");
-		}
-}
+echo "File written\n";
