77a81242f1ec0290fb1d0d348fc54d3e4bbeacd9
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-10 19:11:43 -0500

    Adding models, activerecord package, and config stuff

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+	$config = [
+			'prod' => [
+			'host' => 'http://localhost',
+			'uri' => 'lowphashion',
+			'app_name' => 'lowphashion',
+			'db_host' => '127.0.0.1',
+			'db_name' => 'lowphashion',
+			'db_user' => 'lowphashion',
+			'db_pass' => 'password',
+			'connection' => 'mysql:host=127.0.0.1;dbname=vnnroundme'
+		],
+		'test' => [
+			'host' => 'http://localhost',
+			'uri' => 'lowphashion',
+			'app_name' => 'lowphashion_test',
+			'db_host' => '127.0.0.1',
+			'db_name' => 'lowphashion_test',
+			'db_user' => 'lowphashion',
+			'db_pass' => 'password',
+			'connection' => 'mysql:host=127.0.0.1;dbname=lowphashion_test'
+		]
+];
---
+++ models/School.php
@@ -0,0 +1,3 @@
+<?php
+
+class School extends ActiveRecord\Model {}
