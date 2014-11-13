55d5f4571b9e974083895b065b6fe3d1697286bb
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-12 20:58:48 -0500

    Translating tabs to spaces

--- setup.php
+++ setup.php
@@ -5,12 +5,12 @@ require 'vendor/autoload.php';
 date_default_timezone_set('America/Detroit');
 
 $connections = [
-	'production' => 'mysql://vnn:password@localhost/vnnaroundme'
+    'production' => 'mysql://vnn:password@localhost/vnnaroundme'
 ];
 
 ActiveRecord\Config::initialize(function($config) use ($connections)
 {
-	$config->set_model_directory('models');
-	$config->set_connections($connections);
-	$config->set_default_connection('production');
+    $config->set_model_directory('models');
+    $config->set_connections($connections);
+    $config->set_default_connection('production');
 });
