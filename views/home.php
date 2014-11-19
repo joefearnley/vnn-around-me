31c822bcfeabf77aaa74aca4f6a3225dd35d7293
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-18 21:32:52 -0500

    Updating fonts and adding some content

--- views/home.php
+++ views/home.php
@@ -4,16 +4,14 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <meta name="description" content="VNN Around Me finds the school nearest you that is part of Varsity News Network">
+    <meta name="author" content="Joe Fearnley">
     <title>VNN Around Me</title>
     <link href="assets/css/bootstrap.min.css" rel="stylesheet">
     <link href="assets/css/grayscale.css" rel="stylesheet">
     <link href="assets/font-awesome-4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic" rel="stylesheet" type="text/css">
     <link href="http://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
@@ -51,8 +49,17 @@
     <section id="about" class="container content-section text-center">
         <div class="row">
             <div class="col-lg-8 col-lg-offset-2">
-                <h2>About Grayscale</h2>
-                <p>Grayscale is a free Bootstrap 3 theme created by Start Bootstrap. It can be yours right now, simply download the template on <a href="http://startbootstrap.com/template-overviews/grayscale/">the preview page</a>. The theme is open source, and you can use it for any purpose, personal or commercial.</p>
+                <h2>About VNN Around ME</h2>
+                <p>
+                    VNN Around Me finds the school nearest you that is part of 
+                    <a href="http://varsitynewsnetwork.com/">Varsity News Network</a>. I 
+                    (<a href="htt://joefearnley.com">Joe Fearnley</a>) created it as way to learn
+                    more about HTML5 geolocation.
+                </p>
+                <p>
+                    The source code for it is 
+                    <a href="https://github.com/joefearnley/vnn-around-me">on Github.</a>
+                </p>
             </div>
         </div>
     </section>
@@ -60,7 +67,7 @@
 <script src="assets/js/jquery-1.11.0.js"></script>
 <script src="assets/js/bootstrap.min.js"></script>
 <script src="assets/js/jquery.easing.min.js"></script>
-<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRngKslUGJTlibkQ3FkfTxj3Xss1UlZDA&sensor=false"></script>
 <script src="assets/js/grayscale.js"></script>
 
 </body>
