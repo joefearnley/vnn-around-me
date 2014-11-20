7c53ad55ae7bedc2284d18aad455f77bd488fdee
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-19 22:48:49 -0500

    Updating the way database connections are made

--- setup.php
+++ setup.php
@@ -1,12 +1,9 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'config/database.php';
 
 date_default_timezone_set('America/Detroit');
-
-$connections = [
-    'production' => 'mysql://vnn:password@localhost/vnnaroundme'
-];
 
 ActiveRecord\Config::initialize(function($config) use ($connections)
 {
