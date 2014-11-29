1c576824b0df94cd3094d981a85828ddd895ae68
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-29 10:29:39 -0500

    Adding .htaccess file

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php 
 
 $connections = [
-	'local' => 'mysql://vnn:password@localhost/vnnaroundme',
+	'test' => 'mysql://vnn:password@localhost/vnnaroundme',
     'test' => 'mysql://vnn:password@localhost/vnnaroundme',
     'production' => 'mysql://vnn-around-me:o7EwiABJAznHp0iM@vnn-around-me.mysql.eu1.frbit.com/vnn-around-me'
 ];
