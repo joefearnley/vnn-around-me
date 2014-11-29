a61ff41a45526d5bdca6df757048d613028087c3
Joe Fearnley <joe.fearnley@gmail.com>
2014-11-29 09:17:38 -0500

    Setting enviroment detection

--- start.php
+++ start.php
@@ -9,5 +9,14 @@ ActiveRecord\Config::initialize(function($config) use ($connections)
 {
     $config->set_model_directory('models');
     $config->set_connections($connections);
-    $config->set_default_connection('production');
+
+    $defaultConnection = null;
+    $environment = $_SERVER['HTTP_HOST'];
+    if (strpos($environment,'localhost') !== false) {
+    	$defaultConnection = 'test';
+	} else {
+		$defaultConnection = 'production';
+	}
+
+    $config->set_default_connection($defaultConnection);
 });
